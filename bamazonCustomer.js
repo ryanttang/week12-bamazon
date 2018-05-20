6e829ee7ee654d1af9ace62277f323e8a589c441
rttang <rttang@admins-MacBook-Pro.local>
2018-05-20 10:42:32 -0700

    syntax edits

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ var displayProducts = function() {
     var query = 'SELECT * FROM products'
     connection.query(query, function(err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + " | | Product: " + res[i].product_name + " || Department: " + res[i].department_name +  " || Price: " + res[i].price + " || Stock: " + res[i].stock_quantity);
+            console.log("Item ID: " + res[i].item_id + " | Product: " + res[i].product_name + " | Department: " + res[i].department_name +  " | Price: " + res[i].price + " | Stock: " + res[i].stock_quantity);
         }
         shoppingCart();
     })
@@ -27,7 +27,7 @@ var shoppingCart = function() {
     inquirer.prompt([{ 
         name: "itemID",
         type: "input",
-        message: "What is the ID of the product yuo like to buy?",
+        message: "What is the ID of the product you like to buy?",
         
         validate: function(value) {
             if (isNaN(value) == false) {
@@ -48,15 +48,15 @@ var shoppingCart = function() {
                 }
             }
         }]).then(function(answer) {
-            var query = 'SELECT * FROM Products WHERE itemID =' + answer.Quantity;
+            var query = 'SELECT * FROM Products WHERE item_id =' + answer.Quantity;
             connection.query(query, function(err, res) {
                 if (answer.Quantity <= res) {
                     for (var i = 0; i < res.length; i++) {
-                        console.log("We currently have " + res[i].stockQuantity + " " + res[i].productName + ".");
-                        console.log("Thanks for your order! Your order of "+ res[i].stockQuantity + " " + res[i].productName + " is now being processed.");
+                        console.log("We currently have " + res[i].stock_quantity + " " + res[i].product_name + ".");
+                        console.log("Thanks for your order! Your order of "+ answer.Quantity + " " + res[i].product_name + " is now being processed.");
                     }
                 } else {
-                    console.log("Not enough of this product in stock.") ;
+                    console.log("Sorry! We don't have enough in stock.");
                 }
                 displayProducts();
             })
